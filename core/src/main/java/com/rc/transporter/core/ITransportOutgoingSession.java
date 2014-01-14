297345df6d3bf01f46e772895a43b3abef5599ac
akshay <akshaymdeo@gmail.com>
2014-01-14 08:11:32 +0530

    added documentation on ITransportOutgoingSession class

--- core/src/main/java/com/rc/transporter/core/ITransportOutgoingSession.java
+++ core/src/main/java/com/rc/transporter/core/ITransportOutgoingSession.java
@@ -8,10 +8,21 @@ package com.rc.transporter.core;
  */
 public interface ITransportOutgoingSession<I, O> extends ITransportIncomingSession<I, O> {
 
+    /**
+     * Called when recovery of the outgoing session starts if autorecover is opted
+     */
     public void onRecoveryStarted ();
 
+    /**
+     * Called when recovery is done
+     *
+     * @param channel new @ITransportChannel associated with renewed connection
+     */
     public void onRecovered (ITransportChannel<O> channel);
 
+    /**
+     * Called when all the recovery attempts are failed
+     */
     public void onRecoveryFailed ();
 
 }
