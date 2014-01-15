e905fcb5a84484e37e328046c33e895e184a77bd
akshay <akshaymdeo@gmail.com>
2014-01-15 19:35:17 +0530

    Revert "bug fix"
    This reverts commit 540015439fb0d90ed94a4ba2ac3fd3a92236f67b.

--- netty4x/src/main/java/com/rc/transporter/netty4x/DynamicTransportIncomingSession.java
+++ netty4x/src/main/java/com/rc/transporter/netty4x/DynamicTransportIncomingSession.java
@@ -1,12 +1,12 @@
 package com.rc.transporter.netty4x;
 
-import com.rc.transporter.core.ITransportIncomingSession;
+import com.rc.transporter.core.TransportIncomingSession;
 
 /**
  * Author: akshay
  * Date  : 11/27/13
  * Time  : 1:03 AM
  */
-public abstract class DynamicTransportIncomingSession<I, O>
-        implements ITransportIncomingSession<I, O>, IDynamicTransportIncomingSession<I, O> {
+public abstract class DynamicTransportIncomingSession<I, O> extends TransportIncomingSession<I,
+        O> implements IDynamicTransportIncomingSession<I, O> {
 }
