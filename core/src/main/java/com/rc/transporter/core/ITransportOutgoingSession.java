a01f55984c1c0948b8d926f28ec91c962f680717
akshay <akshaymdeo@gmail.com>
2014-01-15 19:35:18 +0530

    Revert "added documentation on ITransportOutgoingSession class"
    This reverts commit 297345df6d3bf01f46e772895a43b3abef5599ac.

--- core/src/main/java/com/rc/transporter/core/ITransportOutgoingSession.java
+++ core/src/main/java/com/rc/transporter/core/ITransportOutgoingSession.java
@@ -8,21 +8,10 @@ package com.rc.transporter.core;
  */
 public interface ITransportOutgoingSession<I, O> extends ITransportIncomingSession<I, O> {
 
-    /**
-     * Called when recovery of the outgoing session starts if autorecover is opted
-     */
     public void onRecoveryStarted ();
 
-    /**
-     * Called when recovery is done
-     *
-     * @param channel new @ITransportChannel associated with renewed connection
-     */
     public void onRecovered (ITransportChannel<O> channel);
 
-    /**
-     * Called when all the recovery attempts are failed
-     */
     public void onRecoveryFailed ();
 
 }
